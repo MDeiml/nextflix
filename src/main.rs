44a0eb52e6f7c329ae320469e76dcded03f6f253
Matthias Deiml <matthias@deiml.net>
2020-08-28 22:04:43 +0200

    Add actix_web

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ extern crate unic_ucd_category;
 
 mod fts_tree;
 
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
