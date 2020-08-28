920047a5b6f1a0798b50d0ec0e2d782dc0efea5d
Matthias Deiml <matthias@deiml.net>
2020-08-28 22:28:11 +0200

    Add tera

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-extern crate sled;
-extern crate unic_ucd_category;
-
 mod fts_tree;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{error, web, App, Error, HttpResponse, HttpServer, Responder};
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world")
 }
 
+async fn login(tera: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let ctx = tera::Context::new();
+    let body = tera
+        .render("login.html", &ctx)
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(body))
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(index))
+        let tera = tera::Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
+        App::new()
+            .data(tera)
+            .route("/", web::get().to(index))
+            .route("/login", web::get().to(login))
     })
     .bind("127.0.0.1:8080")?
     .run()
