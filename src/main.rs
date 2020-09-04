0d3057f37c0ba90b61fac6abd436c6db37839b7d
Matthias Deiml <matthias@deiml.net>
2020-09-04 15:54:33 +0200

    Error logging

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,52 @@ mod fts_tree;
 mod model;
 
 use actix_identity::{CookieIdentityPolicy, Identity, IdentityService};
-use actix_web::{error, web, App, HttpResponse, HttpServer};
+use actix_web::{error, middleware::Logger, web, App, HttpResponse, HttpServer};
 use database::*;
+use log::debug;
 use model::*;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 type Tera = web::Data<tera::Tera>;
 type Db = web::Data<sled::Db>;
+
+fn log_error<E: std::fmt::Debug>(err: E, message: &'static str) -> error::Error {
+    debug!("{:?}", err);
+    error::ErrorInternalServerError(message)
+}
 
 async fn index(id: Identity, tera: Tera, db: Db) -> actix_web::Result<HttpResponse> {
     let mut ctx = tera::Context::new();
     if let Some(username) = id.identity() {
         let (_user_id, user) = db
             .get_user_by_username(&username)
-            .map_err(|_| error::ErrorInternalServerError("Database error"))?
-            .ok_or(error::ErrorInternalServerError("Authentication error"))?;
+            .map_err(|err| log_error(err, "Database error"))?
+            .ok_or_else(|| {
+                log_error(
+                    format!("User does not exist: {}", username),
+                    "Authentication error",
+                )
+            })?;
         ctx.insert("user", &user);
+        let movie_ids = user
+            .friends
+            .values()
+            .flat_map(|friend_data| &friend_data.movies)
+            .collect::<HashSet<_>>();
+        let movies = movie_ids
+            .iter()
+            .map(|movie_id| -> sled::Result<(String, Movie)> {
+                Ok((movie_id.to_string(), db.get_movie(**movie_id)?.unwrap()))
+            })
+            .collect::<sled::Result<HashMap<_, _>>>()
+            .map_err(|err| log_error(err, "Database error"))?;
+        log::info!("movies: {:?}", &movies);
+        ctx.insert("movies", &movies);
     }
     let body = tera
         .render("index.html", &ctx)
-        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+        .map_err(|err| log_error(err, "Template error"))?;
     Ok(HttpResponse::Ok().body(body))
 }
 
@@ -31,7 +56,7 @@ async fn login(tera: Tera) -> actix_web::Result<HttpResponse> {
     let ctx = tera::Context::new();
     let body = tera
         .render("login.html", &ctx)
-        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+        .map_err(|err| log_error(err, "Template error"))?;
     Ok(HttpResponse::Ok().content_type("text/html").body(body))
 }
 
@@ -48,10 +73,10 @@ async fn login_post(
 ) -> actix_web::Result<HttpResponse> {
     if let Some((_user_id, user)) = db
         .get_user_by_username(&params.username)
-        .map_err(|_| error::ErrorInternalServerError("Database error"))?
+        .map_err(|err| log_error(err, "Database error"))?
     {
         if bcrypt::verify(&params.password, &user.password_hash)
-            .map_err(|_| error::ErrorInternalServerError("Verification error"))?
+            .map_err(|err| log_error(err, "Verification error"))?
         {
             id.remember(user.username);
             return Ok(HttpResponse::Found().header("location", "/").finish());
@@ -72,16 +97,21 @@ async fn logout(id: Identity) -> actix_web::Result<HttpResponse> {
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let private_key = [0u8; 32];
+
+    std::env::set_var("RUST_LOG", "nextflix=debug,actix_web=info");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
     HttpServer::new(move || {
         let tera = tera::Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
         let db = sled::Config::new().temporary(true).open().unwrap();
-        let _pulp_fiction_id = db
+        let pulp_fiction_id = db
             .add_movie(&Movie {
                 name: "Pulp Fiction".to_owned(),
             })
             .unwrap()
             .unwrap();
-        let _admin_id = db
+        let admin_id = db
             .add_user(&User {
                 username: "admin".to_owned(),
                 password_hash: bcrypt::hash("password", bcrypt::DEFAULT_COST).unwrap(),
@@ -89,7 +119,23 @@ async fn main() -> std::io::Result<()> {
             })
             .unwrap()
             .unwrap();
+        let _foo_id = db
+            .add_user(&User {
+                username: "foo".to_owned(),
+                password_hash: bcrypt::hash("1234", bcrypt::DEFAULT_COST).unwrap(),
+                friends: vec![(
+                    admin_id,
+                    FriendData {
+                        movies: vec![pulp_fiction_id],
+                    },
+                )]
+                .into_iter()
+                .collect(),
+            })
+            .unwrap()
+            .unwrap();
         App::new()
+            .wrap(Logger::default())
             .wrap(IdentityService::new(
                 CookieIdentityPolicy::new(&private_key)
                     .name("auth-cookie")
--- src/model.rs
+++ src/model.rs
@@ -1,19 +1,19 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct User {
     pub username: String,
     pub password_hash: String,
     pub friends: HashMap<u64, FriendData>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct FriendData {
     pub movies: Vec<u64>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Movie {
     pub name: String,
 }
