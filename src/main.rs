9db8c2e3aa21eef15adb58fae3a1a7c3d24809e6
Matthias Deiml <matthias@deiml.net>
2020-09-05 16:30:50 +0200

    Add registration

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,6 @@ async fn index(id: Identity, tera: Tera, db: Db) -> actix_web::Result<HttpRespon
             })
             .collect::<sled::Result<HashMap<_, _>>>()
             .map_err(|err| log_error(err, "Database error"))?;
-        log::info!("movies: {:?}", &movies);
         ctx.insert("movies", &movies);
     }
     let body = tera
@@ -52,7 +51,10 @@ async fn index(id: Identity, tera: Tera, db: Db) -> actix_web::Result<HttpRespon
     Ok(HttpResponse::Ok().body(body))
 }
 
-async fn login(tera: Tera) -> actix_web::Result<HttpResponse> {
+async fn login(id: Identity, tera: Tera) -> actix_web::Result<HttpResponse> {
+    if id.identity().is_some() {
+        return Ok(HttpResponse::Found().header("location", "/").finish());
+    }
     let ctx = tera::Context::new();
     let body = tera
         .render("login.html", &ctx)
@@ -94,6 +96,52 @@ async fn logout(id: Identity) -> actix_web::Result<HttpResponse> {
         .finish())
 }
 
+async fn register(id: Identity, tera: Tera) -> actix_web::Result<HttpResponse> {
+    if id.identity().is_some() {
+        return Ok(HttpResponse::Found().header("location", "/").finish());
+    }
+    let ctx = tera::Context::new();
+    let body = tera
+        .render("register.html", &ctx)
+        .map_err(|err| log_error(err, "Template error"))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(body))
+}
+
+#[derive(Serialize, Deserialize)]
+struct RegisterParams {
+    username: String,
+    password: String,
+    password_repeat: String,
+}
+
+async fn register_post(
+    params: web::Form<RegisterParams>,
+    db: Db,
+) -> actix_web::Result<HttpResponse> {
+    if params.password != params.password_repeat {
+        return Ok(HttpResponse::UnprocessableEntity().finish());
+    }
+    if params.password.len() < 8 {
+        return Ok(HttpResponse::UnprocessableEntity().finish());
+    }
+    let RegisterParams {
+        username, password, ..
+    } = params.into_inner();
+    if let Some(id) = db
+        .add_user(&User {
+            username: username,
+            password_hash: bcrypt::hash(password, bcrypt::DEFAULT_COST).unwrap(),
+            friends: HashMap::new(),
+        })
+        .map_err(|err| log_error(err, "Database error"))?
+    {
+        log::info!("register {}", id);
+        Ok(HttpResponse::Found().header("location", "/").finish())
+    } else {
+        Ok(HttpResponse::Conflict().finish())
+    }
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let private_key = [0u8; 32];
@@ -101,39 +149,39 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "nextflix=debug,actix_web=info");
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
+    let db = sled::Config::new().temporary(true).open().unwrap();
+    let pulp_fiction_id = db
+        .add_movie(&Movie {
+            name: "Pulp Fiction".to_owned(),
+        })
+        .unwrap()
+        .unwrap();
+    let admin_id = db
+        .add_user(&User {
+            username: "admin".to_owned(),
+            password_hash: bcrypt::hash("password", bcrypt::DEFAULT_COST).unwrap(),
+            friends: HashMap::new(),
+        })
+        .unwrap()
+        .unwrap();
+    let _foo_id = db
+        .add_user(&User {
+            username: "foo".to_owned(),
+            password_hash: bcrypt::hash("1234", bcrypt::DEFAULT_COST).unwrap(),
+            friends: vec![(
+                admin_id,
+                FriendData {
+                    movies: vec![pulp_fiction_id],
+                },
+            )]
+            .into_iter()
+            .collect(),
+        })
+        .unwrap()
+        .unwrap();
 
     HttpServer::new(move || {
         let tera = tera::Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
-        let db = sled::Config::new().temporary(true).open().unwrap();
-        let pulp_fiction_id = db
-            .add_movie(&Movie {
-                name: "Pulp Fiction".to_owned(),
-            })
-            .unwrap()
-            .unwrap();
-        let admin_id = db
-            .add_user(&User {
-                username: "admin".to_owned(),
-                password_hash: bcrypt::hash("password", bcrypt::DEFAULT_COST).unwrap(),
-                friends: HashMap::new(),
-            })
-            .unwrap()
-            .unwrap();
-        let _foo_id = db
-            .add_user(&User {
-                username: "foo".to_owned(),
-                password_hash: bcrypt::hash("1234", bcrypt::DEFAULT_COST).unwrap(),
-                friends: vec![(
-                    admin_id,
-                    FriendData {
-                        movies: vec![pulp_fiction_id],
-                    },
-                )]
-                .into_iter()
-                .collect(),
-            })
-            .unwrap()
-            .unwrap();
         App::new()
             .wrap(Logger::default())
             .wrap(IdentityService::new(
@@ -142,11 +190,13 @@ async fn main() -> std::io::Result<()> {
                     .secure(false),
             ))
             .data(tera)
-            .data(db)
+            .data(db.clone())
             .route("/", web::get().to(index))
             .route("/login", web::get().to(login))
             .route("/login", web::post().to(login_post))
             .route("/logout", web::get().to(logout))
+            .route("/register", web::get().to(register))
+            .route("/register", web::post().to(register_post))
     })
     .bind("127.0.0.1:8080")?
     .run()
